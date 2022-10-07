93a95233f4e7dbffeb31658512200a0404b92afc
suhsein <mini9075@naver.com>
2022-10-07 11:35:26 +0900

    added divide function and option for issue#1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y
 
-#Need to define divide function.
+# This function divides x by y
 def divide (x,y):
     return x / y
 
@@ -25,10 +25,10 @@ print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3'):
+    if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -40,6 +40,9 @@ while True:
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
+        
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1,num2))
 
         # check if user wants another calculation
         # break the while loop if answer is no
