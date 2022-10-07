5b92c0b3a0c5890277db28796397be08a28fe434
1q2w3e4r11 <kmc_n@naver.com>
2022-10-07 11:52:29 +0900

    add divide funtion for issue#1

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ while True:
             print(num1, "/", num2, "=", multiply(num1, num2))
 
         # check if user wants another calculation
-        # break the while loop if answer is no
+        # break the while loop if answer is no.
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
             break
