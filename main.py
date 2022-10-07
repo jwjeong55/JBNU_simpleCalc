bb3f74f6a8a944c40f6cd3be58fc4605839c9989
yanggyuheon <112867282+yanggyuheon@users.noreply.github.com>
2022-10-07 19:11:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
@@ -41,10 +41,10 @@ while True:
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        else
+        else:
             if num2 == 0:
                 print("0으로 나눌 수 없음")
-            else
+            else:
                 print(num1, "/", num2, "=", divide(num1, num2))
 
         # check if user wants another calculation
