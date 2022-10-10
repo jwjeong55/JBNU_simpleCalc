f68621fbcb83ab0adac0c4e6fa408943aef0cc44
Junhyeong <wnsgudqkr718@gmail.com>
2022-10-10 22:41:23 +0900

    -a
    add divide function for issue#1

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3'):
+    if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -41,6 +41,8 @@ while True:
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
